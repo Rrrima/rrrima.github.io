3103bc8b9f2e45c074224c90b92e3d316e73646a
Rrrima <rimacyn@gmail.com>
2025-11-09 10:58:42 -0800

    Fix prerendering: switch from buggy plugin to react-snap

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+// Check if the page is prerendered (by react-snap)
+if (rootElement.hasChildNodes()) {
+  // If already has content, hydrate instead of render
+  ReactDOM.hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  // Normal render for development
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
