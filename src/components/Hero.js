0d87e8cdc011bc9cb5b9c600d5d22a631ee7b501
Rrrima <rimacyn@gmail.com>
2026-07-04 15:20:49 -0700

    remove .netlify db

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -78,7 +78,7 @@ const Hero = () => {
                 Foundation Interface Lab
               </a>{" "}
               at University of California, San Diego, advised by Professor
-              Haijun Xia
+              Haijun Xia. I am also a Microsoft Research fellow.
             </p>
             <ActivityLog />
           </div>
